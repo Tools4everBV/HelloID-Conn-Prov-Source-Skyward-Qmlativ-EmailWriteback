2c42c979d673dd2bf40186af24ae085a9163fb7a
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2023-07-13 12:11:13 +0200

    fix: URL encoded Client Credentials

--- update.ps1
+++ update.ps1
@@ -34,7 +34,7 @@ else
         )
             Process
             {
-                $pair = $ClientKey + ":" + $ClientSecret;
+                $pair = [System.Web.HTTPUtility]::UrlEncode($ClientKey) + ":" + [System.Web.HTTPUtility]::UrlEncode($ClientSecret)
                 $bytes = [System.Text.Encoding]::ASCII.GetBytes($pair);
                 $bear_token = [System.Convert]::ToBase64String($bytes);
                 $auth_headers = @{ Authorization = "Basic " + $bear_token };
